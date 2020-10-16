5c07c4afb797d09d21b900c8787295ec3f338196
Gallay Lajos <gallay.lajos@gmail.com>
2020-10-16 18:05:10 +0200

    feat(shades-lottie): initial release, updated dependencies

---
+++ packages/shades-lottie/src/index.ts
@@ -0,0 +1,24 @@
+import { PartialElement } from '@furystack/shades'
+import '@lottiefiles/lottie-player'
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  export namespace JSX {
+    interface IntrinsicElements {
+      'lottie-player': {
+        autoplay?: boolean
+        src: string
+        background?: string
+        controls?: boolean
+        count?: number
+        direction?: number
+        hover?: boolean
+        loop?: boolean
+        mode?: string
+        renderer?: 'svg' | 'canvas'
+        speed?: number
+        style?: PartialElement<CSSStyleDeclaration>
+      }
+    }
+  }
+}
